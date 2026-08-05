bdc278ff3ce5a125cd0a072acb1ad57ab4b0bb76
jamiewmeldrum <jamiewmeldrum@gmail.com>
2026-08-05 14:44:38 +0100

    Renamed db migration file to correct for error resulting in table not being added. Added main path QuestionOrigin db test

--- src/test/java/integration/db/QuestionOriginDatabaseIT.java
+++ src/test/java/integration/db/QuestionOriginDatabaseIT.java
@@ -1,12 +1,8 @@
 package integration.db;
 
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
-import com.practiq.domain.types.DocumentStatus;
+import com.practiq.domain.types.QuestionOriginSource;
 import jakarta.inject.Inject;
 import java.time.Instant;
 import java.util.List;
@@ -32,22 +28,16 @@ public class QuestionOriginDatabaseIT {
         long questionId = 4L;
         data.question(questionId).insert();
 
-        data.questionOrigin(questionId)
+        QuestionOriginSource source = QuestionOriginSource.EXTRACTED;
+        data.questionOrigin(questionId, source).insert();
 
-
-        String s3Key = "s3:path";
-        String filename = "file:path";
-        data.document(s3Key, filename).insert();
-
-        List<DBRow> documents = data.retrieveDocuments();
-        DBRow document = documents.getFirst();
-        document.assertThat("id", greaterThan(0L));
-        document.assertThat("version", equalTo(0));
-        document.assertThat("s3_key", equalTo(s3Key));
-        document.assertThat("filename", equalTo(filename));
-        document.assertThat("source_spec", nullValue());
-        document.assertThat("status", equalTo(DocumentStatus.UNAPPROVED.name()));
-        document.assertThat("created_at", allOf(greaterThan(Instant.EPOCH), lessThanOrEqualTo(Instant.now())));
-        document.assertAllColumnsChecked();
+        List<DBRow> questionOrigins = data.retrieveQuestionOrigins();
+        DBRow questionOrigin = questionOrigins.getFirst();
+        questionOrigin.assertThat("id", greaterThan(0L));
+        questionOrigin.assertThat("question_id", equalTo(questionId));
+        questionOrigin.assertThat("source", equalTo(source.name()));
+        questionOrigin.assertThat("document_id", nullValue());
+        questionOrigin.assertThat("created_at", allOf(greaterThan(Instant.EPOCH), lessThanOrEqualTo(Instant.now())));
+        questionOrigin.assertAllColumnsChecked();
     }
 }
