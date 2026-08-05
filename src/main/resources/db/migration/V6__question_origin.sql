1c7e10a0eab418ebbfdb88d0058c380770b06d6f
jamiewmeldrum <jamiewmeldrum@gmail.com>
2026-08-05 15:11:44 +0100

    Added missing DB constraint and fleshed out db tests

--- src/main/resources/db/migration/V6__question_origin.sql
+++ src/main/resources/db/migration/V6__question_origin.sql
@@ -1,7 +1,7 @@
 create table question_origin
 (
     id          bigint generated always as identity primary key,
-    question_id bigint      not null references question (id) on delete cascade,
+    question_id bigint      not null unique references question (id) on delete cascade,
     source      varchar(20) not null check (source in ('AUTHORED', 'EXTRACTED', 'GENERATED')),
     document_id bigint references document (id),
     created_at  timestamptz not null default now()
