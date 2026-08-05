bb642f223bd92d242c54216d5eaf30b934ab98ff
jamiewmeldrum <jamiewmeldrum@gmail.com>
2026-08-05 11:59:54 +0100

    Added Document domain entity, basic repository and IT covering versioning

---
+++ src/main/java/com/practiq/domain/Document.java
@@ -0,0 +1,42 @@
+package com.practiq.domain;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import com.practiq.domain.types.DocumentStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import java.time.Instant;
+import lombok.Getter;
+import lombok.ToString;
+import org.hibernate.annotations.Generated;
+
+@Entity
+@Table(name = "document")
+@Getter
+@ToString
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    @Version
+    private int version;
+
+    @NotNull @Column(name = "s3_key", nullable = false)
+    private String s3Key;
+
+    @NotNull @Column(name = "filename", nullable = false)
+    private String filename;
+
+    @Column(name = "source_spec")
+    private String sourceSpec;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private DocumentStatus status;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    @Generated
+    private Instant createdAt;
+}
